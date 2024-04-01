16fa19eaef91bd4910329c4c8ebfa4c89a32578b
Anastasia Kolesnyk <ana.ko8915@gmail.com>
2024-04-02 02:45:38 +0300

    testimonials slider

--- src/components/MiscElements/Testimonials.tsx
+++ src/components/MiscElements/Testimonials.tsx
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "./Testimonials.scss";
 import TestemonialCard from "./TestemonialCard";
-import { Swiper, SwiperSlide } from "swiper/react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Testimonials = () => {
+  const slider = useRef<Slider>(null);
+  const [currentSlide, setCurrentSlide] = useState<number>(0);
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 800,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    swipeToSlide: true,
+    arrows: false,
+    draggable: false,
+    adaptiveHeight: true,
+    beforeChange: (current: number, next: number) => setCurrentSlide(next),
+    afterChange: (index: number) => {
+      console.log("Slider Changed to: ", index);
+    },
+  };
+
+  const isOffFrameSlide = (index: number) => index === (currentSlide + 1) % 2;
+
   return (
     <div className="testimonials-container">
       <div className="testimonials-head-wrapper">
@@ -15,18 +38,32 @@ const Testimonials = () => {
           />
           <p>Testimonials</p>
         </div>
-        <div className="slider-wrapper">
-          <TestemonialCard
-            authorPicSrc="/frame-86@2x.png"
-            authorName="– Luna, Luna’s Asethetics"
-            testimonialText="Porttitor mattis congue gravida consectetur duis euismod imperdiet. Nunc eu amet, adipiscing amet tincidunt mi lectus ut. Habitant nullam ut auctor."
-          />
-          <TestemonialCard
-            authorPicSrc="/frame-86-1@2x.png"
-            authorName="– Pan Roman, Chloé Asethetics"
-            testimonialText="At semper elementum sapien amet. Orci imperdiet habitant purus sit aliquet in. Tortor mauris morbi eget lobortis cras malesuada. Non fringilla vulputate sed"
-          />
-        </div>
+        <Slider ref={slider} {...settings} className="slider-wrapper">
+          {[0, 1].map((_, index: number) => (
+            <div
+              key={index}
+              onClick={() => {
+                if (isOffFrameSlide(index)) {
+                  slider.current?.slickNext();
+                }
+              }}
+            >
+              <TestemonialCard
+                authorPicSrc={`/frame-86${index % 2 === 0 ? "" : "-1"}@2x.png`}
+                authorName={`– ${
+                  index % 2 === 0
+                    ? "Luna, Luna’s Asethetics"
+                    : "Pan Roman, Chloé Asethetics"
+                }`}
+                testimonialText={`${
+                  index % 2 === 0
+                    ? "Porttitor mattis congue gravida consectetur duis euismod imperdiet. Nunc eu amet, adipiscing amet tincidunt mi lectus ut. Habitant nullam ut auctor."
+                    : "At semper elementum sapien amet. Orci imperdiet habitant purus sit aliquet in. Tortor mauris morbi eget lobortis cras malesuada. Non fringilla vulputate sed"
+                }`}
+              />
+            </div>
+          ))}
+        </Slider>
       </div>
     </div>
   );
