2b18d0eb5119490ff0e79a41af01fd1756579735
Anastasia Kolesnyk <ana.ko8915@gmail.com>
2024-04-01 12:18:25 +0300

    fixed animations in cards

--- src/components/CardsSection.tsx
+++ src/components/CardsSection.tsx
@@ -10,17 +10,20 @@ const CardsSection = () => {
           title="Our 1-day design services"
           content="I NEED A VISUAL REVAMP|ASAP"
           imgSrc="/img-11@2x.png"
+          bgImgSrc="/card-bg-img.png"
         />
         <CardComponent
           title="Our retainer services"
           content="I NEED AN|EXTERNAL
           MARKETING TEAM"
           imgSrc="/img-1-1@2x.png"
+          bgImgSrc="/img-11@2x.png"
         />
         <CardComponent
           title="Let’s work together"
           content="I NEED TO|INCREASE ONLINE PRESENCE"
           imgSrc="/img-2@2x.png"
+          bgImgSrc="img-1-1@2x.png"
         />
       </div>
     </section>
--- src/components/MiscElements/CardComponent.tsx
+++ src/components/MiscElements/CardComponent.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./CardComponent.scss";
 import ChatButton from "./ChatButton";
 
@@ -6,14 +6,26 @@ interface CardProps {
   title: string;
   content: string;
   imgSrc: string;
+  bgImgSrc: string;
 }
 
-const CardComponent: React.FC<CardProps> = ({ title, content, imgSrc }) => {
+const CardComponent: React.FC<CardProps> = ({
+  title,
+  content,
+  imgSrc,
+  bgImgSrc,
+}) => {
   const contentLines = content
     .split("|")
     .map((line, index) => <p key={index}>{line}</p>);
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
-    <div className="work-card">
+    <div
+      className="work-card"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
       <div className="text">
         <div className="title">{title}</div>
         <h1 className="content">{contentLines}</h1>
@@ -21,10 +33,15 @@ const CardComponent: React.FC<CardProps> = ({ title, content, imgSrc }) => {
       <div className="imgs-wrapper">
         <div className="imgs">
           <img className="img" loading="lazy" alt={title} src={imgSrc} />
+          <img className="bg-img" src={bgImgSrc} loading="lazy" alt={title} />
         </div>
       </div>
       <div className="button-wrapper">
-        <ChatButton buttonText="tell me more" width="165px" />
+        <ChatButton
+          buttonText="tell me more"
+          width="165px"
+          parentHover={isHovered}
+        />
       </div>
     </div>
   );
