8498c184eadb055c3f2e4ce1937e3a0a4ad4db2d
Anastasia Kolesnyk <ana.ko8915@gmail.com>
2024-04-05 13:23:14 +0300

    mobile highlights

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -20,8 +20,8 @@ const Home: React.FC = () => {
       <HowWeWork />
       <CardsSection />
       <ProcessesSection />
-      {/* <Highlights />
-      <Testimonials />
+      <Highlights />
+      {/* <Testimonials />
       <GetInTouchSection />
       <Footer /> */}
     </>
