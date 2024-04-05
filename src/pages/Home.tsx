20f06effc5bd4e9586952938753640443f10ce19
Anastasia Kolesnyk <ana.ko8915@gmail.com>
2024-04-06 00:22:45 +0300

    mobile get-in-touch

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -22,8 +22,8 @@ const Home: React.FC = () => {
       <ProcessesSection />
       <Highlights />
       <Testimonials />
-      {/* <GetInTouchSection />
-      <Footer /> */}
+      <GetInTouchSection />
+      {/* <Footer /> */}
     </>
   );
 };
